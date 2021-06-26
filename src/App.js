5ee4e5691dfe01982a32420c1864dc07664987df
gentledot <gentledot.wp@gmail.com>
2021-06-26 18:08:16 +0900

    react 기본기 익히기 : 브라우저 히스토리 API 활용방법 확인(2)_react-router-dom 사용하기

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
 import './App.css';
-import React, {useEffect, useState} from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Rooms from './Rooms';
 
 function App() {
-  const [pageName, setPageName] = useState('');
-  
-  useEffect(() => {
-      let onpopstate = event => {
-          console.log(`location: ${document.location}, state: ${event.state}`);
-          setPageName(event.state);
-      };
-      window.onpopstate = onpopstate
-  }, []);
-
-  function onClick1() {
-      const pageName = 'page1';
-      window.history.pushState(pageName, '', '/page1');
-      setPageName(pageName);
-  }
-
-  function onClick2() {
-      const pageName = 'page2';
-      window.history.pushState(pageName, '', '/page2');
-      setPageName(pageName);
-  }
-
-  return (
-      <div>
-          <button onClick={onClick1}> page1 </button>
-          <button onClick={onClick2}> page2 </button>
-          {!pageName && <Home />}
-          {pageName === 'page1' && <Page1 />}
-          {pageName === 'page2' && <Page2 />}
-      </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <Link to="/">홈</Link>
+                <br/>
+                <Link to="/photo">사진</Link>
+                <br/>
+                <Link to="/rooms">방 소개</Link>
+                <br/>
+                <Route exact path="/" component={Home} />
+                <Route path="/photo" component={Photo} />
+                <Route path="/rooms" component={Rooms} />
+            </div>
+        </BrowserRouter>
+    )  
 }
 
-function Home() {
-    return <h2>여기는 홈페이지입니다. 원하는 페이지 버튼을 클릭하세요.</h2>;
+function Home({match}) {
+    return <h2>이곳은 홈페이지입니다.</h2>
 }
 
-function Page1() {
-    return <h2>여기는 Page1 입니다.</h2>;
-}
-
-function Page2() {
-    return <h2>여기는 Page2 입니다.</h2>
+function Photo({match}) {
+    return <h2>사진 감상은 여기서.</h2>
 }
 
 export default App;
