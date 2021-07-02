450e814c2cfc4c20246a29f48bd4434cadf2b24d
gentledot <gentledot.wp@gmail.com>
2021-07-02 20:36:31 +0900

    simple-shopping-front : material-ui로 ui 제작 시작, Header에 AppBar 영역 (샘플) 적용

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 const Header = () => {
+    const classes = useStyles();
+
     return (
         <div className="header">
-            <div className="row bg-info">
-                <a href="/">
-                    <span>Gentledot</span>
-                </a>
-            </div>
+            <AppBar position="relative">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Gentledot
+                    </Typography>
+                    <Button color="inherit">Home</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import 'normalize.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
