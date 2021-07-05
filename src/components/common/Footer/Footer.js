7970e95b2022a5e4c4918b889b77655aa83ba6e9
gentledot <gentledot.wp@gmail.com>
2021-07-05 21:49:49 +0900

    simple-shopping-front : Header (AppBar), Footer(BottomNavigation) 생성(2)

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import {BottomNavigation, BottomNavigationAction, makeStyles} from "@material-ui/core";
+import {BottomNavigation, BottomNavigationAction, makeStyles, Typography} from "@material-ui/core";
 import {HomeOutlined, MenuBookOutlined, PeopleAltOutlined} from "@material-ui/icons";
 
-const useStyles = makeStyles({
-
-});
+const useStyles = makeStyles((theme) => ({
+    madeBy: {
+        ...theme.typography.button,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(1),
+        textAlign: 'right',
+    }
+}));
 
 const Footer = () => {
     const classes = useStyles();
     const [value, setValue] = React.useState(0);
 
     return (
-        <BottomNavigation
-            value={value}
-            onChange={(event, newValue) => {
-                setValue(newValue);
-            }}
-            showLabels
-        >
-            <BottomNavigationAction label='홈으로' icon={<HomeOutlined />} />
-            <BottomNavigationAction label='회원정보' icon={<PeopleAltOutlined />} />
-            <BottomNavigationAction label='상품정보' icon={<MenuBookOutlined />} />
-        </BottomNavigation>
+        <>
+            <BottomNavigation
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+                showLabels
+            >
+                <BottomNavigationAction label='홈으로' icon={<HomeOutlined/>}/>
+                <BottomNavigationAction label='회원정보' icon={<PeopleAltOutlined/>}/>
+                <BottomNavigationAction label='상품정보' icon={<MenuBookOutlined/>}/>
+            </BottomNavigation>
+            <div>
+                <Typography className={classes.madeBy} variant='subtitle2'> created by gentledot </Typography>
+            </div>
+        </>
     );
 };
 
